cd40e728c0346d42c340405f3b2319fa6d477b27
Timothy <timothyletsoko3@gmail.com>
2023-11-27 10:35:41 +0200

    Initial commit

---
+++ src/main/java/com/timothy/hotels/entity/Hotel.java
@@ -0,0 +1,23 @@
+package com.timothy.hotels.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "hotel")
+public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String location;
+    private double price;
+}
